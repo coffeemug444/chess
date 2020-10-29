a34833e633112bc58439954033bbface0cdae511
Ben Slattery <bsl28@uclive.ac.nz>
2020-10-30 00:24:51 +1300

    added inital class files

---
+++ board.h
@@ -0,0 +1,27 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+enum Pieces {
+    BK = 0, // Black King
+    BQ,     // Black Queen
+    BB,     // Black Bishop
+    BN,     // Black Knight
+    BR,     // Black Rook
+    BP,     // Black Pawn
+
+    WK,     // White King
+    WQ,     // White Queen
+    WB,     // White Bishop
+    WN,     // White Knight
+    WR,     // White Rook
+    WP      // White Pawn
+};
+
+typedef enum Pieces Piece;
+
+class Board {
+
+};
+
+
+#endif
