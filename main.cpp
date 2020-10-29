2b276220ff08bd2b910dca8d9551919035599325
Ben Slattery <bsl28@uclive.ac.nz>
2020-10-30 09:42:57 +1300

    made graphics work

--- main.cpp
+++ main.cpp
@@ -1,50 +1,47 @@
 #include "board.h"
 #include <vector>
 #include <SFML/Graphics.hpp>
-#include "stdlib.h"
+#include "stdio.h"
 
 
-
-void updateSprites(std::vector <sf::Sprite*> &sprites, sf::IntRect pieceRects[12], sf::Texture *spriteTexture, Board *board) {
-    for (sf::Sprite *sprite : sprites) {
-        free(sprite);
-    }
-    sprites.clear();
-    Board::Piece state[16][16];
-    board->getState(state);
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (state[i + 1][j + 1] != Board::EM) {
-                sf::Sprite* x = (sf::Sprite*)malloc(sizeof(sf::Sprite));
-                x->setTexture(*spriteTexture);
-                x->setTextureRect(pieceRects[state[i][j]]);
-                x->move(i * 45.f, (7 - j)*45.f);
-            }
-        }
-    }
-
-}
 
 int main (int argc, char **argv) {
     sf::RenderWindow window(sf::VideoMode(360, 360), "Chess");
     sf::Texture boardTex;
     boardTex.loadFromFile("assets/board.png");
     sf::Sprite boardSprite(boardTex);
+    //boardSprite.setTextureRect(sf::IntRect(0,0,360,360));
+
+    
     sf::Texture piecesTex;
     piecesTex.loadFromFile("assets/pieces.png");
+    
     sf::IntRect pieceRects[12];
+    
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 6; j++) {
             sf::IntRect pieceRect(0, 0, 45, 45);
             pieceRect.left = 45 * j;
             pieceRect.top = 45 * i;
-            pieceRects[j - i * 6] = pieceRect;
+
+            pieceRects[6 + j - 6 * i] = pieceRect;
         }
     }
+
     
     Board* board = new Board();
-    std::vector <sf::Sprite*> sprites;
-    updateSprites(sprites, pieceRects, &piecesTex, board);
+    std::vector <sf::Sprite> sprites;
+    Board::Piece state[16][16];
+    board->getState(state);
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (state[i][j] != Board::EM) {
+                sprites.push_back(sf::Sprite(piecesTex));
+                sprites.back().setTextureRect(pieceRects[state[i][j]]);
+                sprites.back().setPosition(45*i, 45*j);
+            }
+        }
+    }
 
     while (window.isOpen())
     {
@@ -57,10 +54,11 @@ int main (int argc, char **argv) {
 
         window.clear();
         window.draw(boardSprite);
-        for (sf::Sprite *sprite : sprites) {
-            window.draw(*sprite);
+        
+        for (sf::Sprite sprite : sprites) {
+            window.draw(sprite);
         }
-        //window.draw(shape);
+        
         window.display();
     }
 
