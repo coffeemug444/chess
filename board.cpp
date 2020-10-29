58e13de189ac4b87440ab3b8b307e4e09bde794f
Ben Slattery <bsl28@uclive.ac.nz>
2020-10-30 01:18:01 +1300

    added some functions definitions to board

--- board.cpp
+++ board.cpp
@@ -1,1 +1,40 @@
 #include "board.h"
+
+Board::Board () {
+    reset();
+}
+
+void Board::reset () {
+    turn = WHITE;
+
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 16; j++) {
+            state_ [i][j] = starting_board_ [i][j];
+        }
+    }
+}
+
+Board::Piece Board::getPiece (Pos pos) {
+    return state_ [pos.x][pos.y];
+}
+
+int Board::doMove (Piece piece, Pos startPos, Pos endPos) {
+    if (piece == EM) {
+        // if player tries to move an empty piece
+        return -1;
+    }
+    if (turn == WHITE && piece <= BP) {
+        // if it's white's turn and player tries to move a black piece
+        return -1;
+    }
+    if (turn == BLACK && piece >= WK) {
+        // if it's black's turn and player tries to move a white piece
+        return -1;
+    }
+    if (getPiece(startPos) != piece) {
+        // if the piece the player is trying to move is not in the starting position
+        return -1;
+    }
+
+    // player has selected a valid piece. Check to see if the place they're moving it to is valid.
+}
--- board.h
+++ board.h
@@ -1,26 +1,60 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-enum Pieces {
-    BK = 0, // Black King
-    BQ,     // Black Queen
-    BB,     // Black Bishop
-    BN,     // Black Knight
-    BR,     // Black Rook
-    BP,     // Black Pawn
-
-    WK,     // White King
-    WQ,     // White Queen
-    WB,     // White Bishop
-    WN,     // White Knight
-    WR,     // White Rook
-    WP      // White Pawn
-};
-
-typedef enum Pieces Piece;
-
 class Board {
+public:
+    // type declarations
 
+    typedef enum Piece {
+        BK = 0, // Black King
+        BQ,     // Black Queen
+        BB,     // Black Bishop
+        BN,     // Black Knight
+        BR,     // Black Rook
+        BP,     // Black Pawn
+
+        WK,     // White King
+        WQ,     // White Queen
+        WB,     // White Bishop
+        WN,     // White Knight
+        WR,     // White Rook
+        WP,     // White Pawn
+
+        EM      // Empty
+    } Piece;
+
+    typedef enum Player {
+        BLACK = 0,
+        WHITE
+    } Player;
+
+    typedef struct Pos {
+        int x;
+        int y;
+    } Pos;
+
+private:
+    Piece starting_board_ [16][16] = { {BR, BN, BB, BQ, BK, BB, BN, BR},
+                                       {BP, BP, BP, BP, BP, BP, BP, BP},
+                                       {EM, EM, EM, EM, EM, EM, EM, EM},
+                                       {EM, EM, EM, EM, EM, EM, EM, EM},
+                                       {EM, EM, EM, EM, EM, EM, EM, EM},
+                                       {EM, EM, EM, EM, EM, EM, EM, EM},
+                                       {WP, WP, WP, WP, WP, WP, WP, WP},
+                                       {WR, WN, WB, WQ, WK, WB, WN, WR}
+                                     };
+    Piece state_ [16][16];  // 16*16 array of the board
+    Player turn;            // current players turn
+
+
+public:
+    Board ();
+    void getState (Piece state [16][16]);   // copies the current state of the board into the array pointed to
+    Piece getPiece (Pos pos); // get piece at pos
+    int doMove (Piece piece, Pos startPos, Pos endPos); // attempts to play the input move. Returns 0 on success, -1 on failure (invalid move), 1 if they won the game
+    bool isCheckmate ();
+    void undoMove ();
+    void reset ();
 };
 
 
